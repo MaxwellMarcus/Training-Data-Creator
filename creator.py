d494b46bcba8bdffcb271ef68867f4a4e5b070bb
Maxwell Marcus <bigmaxmarcus@gmail.com>
2020-06-02 16:12:39 -0500

    added an error message

--- creator.py
+++ creator.py
@@ -4,6 +4,7 @@ import numpy as np
 
 filename = 'testDicom.dcm'
 
+#get pixel data from image
 ds = pydicom.read_file(filename, force=True)
 image = ds.pixel_array
 
@@ -11,6 +12,7 @@ new_img = []
 max_value = None
 min_value = None
 
+#get maximum and minimum pixel values
 for i in image:
     for l in i:
         if max_value:
@@ -25,12 +27,15 @@ for i in image:
         else:
             min_value = l
 
+#use maximum and minimum pixel values to map pixel values between 0 and 255
 for i in image:
     row = []
     for pixel in i:
         row.append((pixel - min_value) / (max_value / 255.0))
     new_img.append(row)
 
+#convert to numpy array
 new_img = np.array(new_img)
 
+#save image
 cv2.imwrite(filename.replace('.dcm', '.jpg'), new_img)
