35602be4c01f5cc55c66c8e9ab72a8fdc3b979bb
Maxwell Marcus <bigmaxmarcus@gmail.com>
2020-06-03 11:46:07 -0500

    Started adding text onto images

--- creator.py
+++ creator.py
@@ -5,8 +5,14 @@ import pylibjpeg
 from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
+from PIL import ImageOps
+import sys
+import random
+import os
+import json
+import string
 
-filename = 'testDicom2.dcm'
+filename = 'dicomImage.dcm'
 
 #get pixel data from image
 ds = pydicom.read_file(filename)
@@ -16,6 +22,7 @@ image = ds.pixel_array
 new_img = []
 max_value = None
 min_value = None
+
 
 #get maximum and minimum pixel values
 if (len(image.shape) == 2):
@@ -48,11 +55,97 @@ else:
     cv2.imwrite('tempImage.jpg', image[0])
 
 image = Image.open('tempImage.jpg')
-draw = ImageDraw.Draw(image)
-font = ImageFont.truetype('Fonts/Arial.ttf', 150)
-try:
-    draw.text((0, 0), 'Test Text', (255, 255, 255), font=font)
-except:
-    draw.text((0, 0), 'Test Text', 255, font=font)
 
-image.save('finalImage.jpg')
+noTextImages = []
+textImages = []
+
+noTextImages.append(ImageOps.flip(image))
+noTextImages.append(ImageOps.mirror(image))
+noTextImages.append(ImageOps.mirror(ImageOps.flip(image)))
+
+
+fonts = [ImageFont.truetype('Fonts/' + font, int(image.width/35)) for font in os.listdir('Fonts')]
+colors = ['white', 'orange', 'gray', 'green', 'red']
+textLocations = [
+{'x': 50, 'y': 50, 'anchor': 'NW'},
+{'x': 50, 'y': image.height - 50, 'anchor': 'SW'},
+{'x': image.width - 50, 'y': 50, 'anchor': 'NE'},
+{'x': image.width - 50, 'y': image.height - 50, 'anchor': 'SE'},
+]
+textOptions = {
+'Patient Name: ': [True, '* *', '*, *'],
+'Patient Sex: ': [True, 'Male', 'Female', 'M', 'F'],
+'DateTime': [False, '||/||/||||', '||||/||/||', '* ||, ||||', '||/||/|||| ||:||:|| AM', '||/||/|||| ||:||:|| PM'],
+'Comments: ': [True, None],
+'ScanData': [False, None],
+'Im: ': [True, '||/||'],
+'Se: ': [True, '||'],
+'WL: ': [True, '||| WW: ||||'],
+'T: ': [True, '|.|mm L: ||.|mm'],
+'FS: ': [True, '|.|'],
+'TR: ': [True, '||||.| TE: |||.|']
+}
+
+for imageNum in range(3):
+    new_img = image.copy()
+    draw = ImageDraw.Draw(new_img)
+    data = []
+    usedTextLocations = []
+    font = fonts[random.randint(0, len(fonts) - 1)]
+    color = colors[random.randint(0, len(colors) - 1)]
+    for textSection in range(random.randint(1, 4)):
+        textLocation = textLocations[random.randint(0, len(textLocations) - 1)]
+        if not textLocation in usedTextLocations:
+            usedTextLocations.append(textLocation)
+            x = textLocation['x']
+            y = textLocation['y']
+            usedTextOptions = []
+            text = ''
+            for textLines in range(random.randint(1, 4)):
+                textOption = list(textOptions)[random.randint(0, len(textOptions) - 1)]
+                if not textOption in usedTextOptions:
+                    usedTextOptions.append(textOption)
+                    textOptionOption = textOptions[textOption][random.randint(1, len(textOptions[textOption]) - 1)]
+
+                    if textOptions[textOption][0] == True:
+                        text += textOption
+
+                    if textOptionOption == None:
+                        for word in range(random.randint(3, 5)):
+                            for letter in range(random.randint(3, 7)):
+                                text += random.choice(string.ascii_letters.lower())
+                            text += ' '
+                    else:
+                        for char in textOptionOption:
+                            if char == '|':
+                                text += str(random.randint(0, 9))
+                            elif char == '*':
+                                for letter in range(random.randint(3, 7)):
+                                    text += random.choice(string.ascii_letters.lower())
+                            else:
+                                text += char
+                text += '\n'
+
+            width, height = draw.multiline_textsize(text, font)
+            if 'S' in textLocation['anchor']:
+                y -= height
+
+            if 'E' in textLocation['anchor']:
+                x -= width
+
+            draw.multiline_text((x, y), text, color, font)
+            bbox = {'x0': x, 'y0': y, 'x1': x + width, 'y1': y + width}
+            data.append({'bbox': bbox, 'text': text})
+
+    textImages.append([new_img, data])
+
+
+for image in range(len(textImages)):
+    try:
+        os.mkdir('Output Images/' + str(image))
+    except:
+        print('New directory not created')
+
+    textImages[image][0].save('Output Images/' + str(image) + '/finalImage.jpg')
+    with open('Output Images/' + str(image) + '/data.json', 'w+') as file:
+        json.dump(textImages[image][1], file)
